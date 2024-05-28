3a73b1c60d90debc602cc5fb5907ec83aa7a2fbf
Amrit478 <123419466+Amrit478@users.noreply.github.com>
2024-05-28 17:21:31 -0400

    made Account.java as an Abstract class

--- Account.java
+++ Account.java
@@ -1,2 +1,19 @@
 public abstract class Account {
 
+private int Balance;
+private int Expense;
+private int Deposit;
+
+Public Account(int balance, int expense, int deposit) {
+this.Balance = balance;
+this.Expense = expense;
+this.Deposit = deposit;
+}
+
+//Getters 
+
+public abstract double getbalance();
+
+public abstract double getexpense();
+
+public abstract double getdeposit();
