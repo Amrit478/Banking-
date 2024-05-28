dd907e40f33543c84d85bcce9f603d0772be8c76
Amrit478 <123419466+Amrit478@users.noreply.github.com>
2024-05-28 17:07:52 -0400

    This is Main class

---
+++ Main.java
@@ -0,0 +1,19 @@
+Public class Main {
+
+public static main void(String[] args) {
+
+Banking banking = new Bankings();
+
+Account account = new Account();
+
+Customer customer = new Customer();
+
+Credit credit = new Credit();
+
+Debit debit = new Debit();
+
+Transfer transfer = new Transfer();
+
+Bills bill = new Bills();
+
+
